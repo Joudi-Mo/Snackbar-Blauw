eb1a74231f33a3219b3d165bdc9d6fb864e4d4bc
Joudi Mohamad <jr.mm65y@gmail.com>
2022-09-28 13:01:25 +0200

    sql(role) gefixt

--- Classes/DbSeeder.php
+++ Classes/DbSeeder.php
@@ -2,7 +2,6 @@
 require 'Database.php';
 require_once '../vendor/autoload.php';
 
-
 class DbSeeder
 {
     private $conn;
@@ -18,27 +17,50 @@ class DbSeeder
     {
 
         $statement = "INSERT INTO 
-                                `user`(`firstname`, `lastname`, `email`)
-                     VALUES ";
-
-        // ("' . $this->faker->firstName() . '", "' . $this->faker->lastName() . ')" ;
-
-
-
-
+                            -- `user`(`firstname`, `lastname`, `phonenumber`, `email`, `password`, `role`)
+                            -- `user`(`firstname`, `lastname`, `phonenumber`, `email`, `password`)
+                            `user`(`firstname`, `lastname`, `email`, `role`)
+                      VALUES ";
 
         for ($i = 0; $i < $number_of_records; $i++) {
-            if($i == $number_of_records - 1){
+            if ($i == $number_of_records - 1) {
 
-                $statement .= "('{$this->faker->firstName()}', '{$this->faker->lastName()}', '{$this->faker->email()}')";
+                // $statement .= "('{$this->faker->firstName()}', '{$this->faker->lastName()}', '{$this->faker->email()}')";
+                $statement .= "('{$this->faker->firstName()}', '{$this->faker->lastName()}', '{$this->faker->email()}', '{$this->faker->randomElement(['Klant', 'Medewerker', 'Manager'])}')";
+                // $statement .= '("Peter","Van Dijk","987654321","peter@test.com","123")';
+                // $statement .= "(
+                //     '{$this->faker->firstName()}',
+                //     '{$this->faker->lastName()}',
+                //     '{$this->faker->phoneNumber()}',
+                //     '{$this->faker->email()}',
+                //     '{$this->faker->password()}'
+                //     )"; 
+            } else {
+
+                // $statement .= "('{$this->faker->firstName()}', '{$this->faker->lastName()}', '{$this->faker->email()}'),";
+                $statement .= "('{$this->faker->firstName()}', '{$this->faker->lastName()}', '{$this->faker->email()}', '{$this->faker->randomElement(['Klant', 'Medewerker', 'Manager'])}'),";
+                // $statement .= '("Peter","Van Dijk","987654321","peter@test.com","123"),';
+                // $statement .= "(
+                //     '{$this->faker->firstName()}',
+                //     '{$this->faker->lastName()}',
+                //     '{$this->faker->phoneNumber()}',
+                //     '{$this->faker->email()}',
+                //     '{$this->faker->password()}'
+                // ),"; 
             }
-            else{
+        } //End for loop
 
-                $statement .= "('{$this->faker->firstName()}', '{$this->faker->lastName()}', '{$this->faker->email()}'),";
-            }
-        }
+        // $statement .= "(
+        //     '{$this->faker->firstName()}',
+        //     '{$this->faker->lastName()}',
+        //     '{$this->faker->phoneNumber()}',
+        //     '{$this->faker->email()}',
+        //     '{$this->faker->password()}',
+        //     '{$this->faker->randomElement(['Klant', 'Medewerker', 'Manager'])};
 
-        // $state
+        //     // 'c''
+        // ),"; 
+
 
         // $sql ='INSERT INTO 
         //         `user`(`firstname`, `lastname`, `phonenumber`, `email`, `password`, `role`)
@@ -48,10 +70,11 @@ class DbSeeder
         //         ("Mark","Goot","0101010101","Mark@test.com","123","Manager")';
 
         mysqli_query($this->conn, $statement);
-
         print_r(mysqli_info($this->conn));
     }
 }
 
 $seeder = new DbSeeder($conn);
-$seeder->fillUsers(10);
+$seeder->fillUsers(3);
+// echo '<button onclick ="$seeder->fillUsers(10);">FillUsers</button>';
+?>
