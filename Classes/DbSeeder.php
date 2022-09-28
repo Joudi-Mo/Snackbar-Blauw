1062394074c801ff06f9c227b3f61da2dd36368d
Joudi Mohamad <jr.mm65y@gmail.com>
2022-09-28 12:01:23 +0200

    DBseeder verder ontwikkelt

--- Classes/DbSeeder.php
+++ Classes/DbSeeder.php
@@ -2,39 +2,56 @@
 require 'Database.php';
 require_once '../vendor/autoload.php';
 
-// use the factory to create a Faker\Generator instance
-$faker = Faker\Factory::create();
-// generate data by calling methods
-echo $faker->name();
-// 'Vince Sporer'
-echo $faker->email();
-// 'walter.sophia@hotmail.com'
-echo $faker->text();
-// 'Numquam ut mollitia at consequuntur inventore dolorem.'
 
 class DbSeeder
 {
+    private $conn;
+    private $faker;
+
     public function __construct($conn)
     {
+        $this->faker = Faker\Factory::create();
         $this->conn = $conn;
     }
 
-    public function fillUsers(){
+    public function fillUsers(int $number_of_records)
+    {
 
-        $statement = 'INSERT INTO 
-                    `user`(`firstname`, `lastname`, `phonenumber`, `email`, `password`, `role`)
-                    VALUES ';
-        for ($i=0; $i < 10; $i++) { 
-            $statement .= ("Peter","Van Dijk","123456789","peter@test.com","123","Klant");
+        $statement = "INSERT INTO 
+                                `user`(`firstname`, `lastname`, `email`)
+                     VALUES ";
+
+        // ("' . $this->faker->firstName() . '", "' . $this->faker->lastName() . ')" ;
+
+
+
+
+
+        for ($i = 0; $i < $number_of_records; $i++) {
+            if($i == $number_of_records - 1){
+
+                $statement .= "('{$this->faker->firstName()}', '{$this->faker->lastName()}', '{$this->faker->email()}')";
+            }
+            else{
+
+                $statement .= "('{$this->faker->firstName()}', '{$this->faker->lastName()}', '{$this->faker->email()}'),";
+            }
         }
-        $sql ='INSERT INTO 
-                `user`(`firstname`, `lastname`, `phonenumber`, `email`, `password`, `role`)
-            VALUES 
-                ("Peter","Van Dijk","987654321","peter@test.com","123","Klant"),
-                ("Jan","Van de Beek","123456789","jan@test.com","123","Medewerker"),
-                ("Mark","Goot","0101010101","Mark@test.com","123","Manager")';
-        
-        mysqli_query($this->conn, $sql);
 
+        // $state
+
+        // $sql ='INSERT INTO 
+        //         `user`(`firstname`, `lastname`, `phonenumber`, `email`, `password`, `role`)
+        //     VALUES 
+        //         ("Peter","Van Dijk","987654321","peter@test.com","123","Klant"),
+        //         ("Jan","Van de Beek","123456789","jan@test.com","123","Medewerker"),
+        //         ("Mark","Goot","0101010101","Mark@test.com","123","Manager")';
+
+        mysqli_query($this->conn, $statement);
+
+        print_r(mysqli_info($this->conn));
     }
 }
+
+$seeder = new DbSeeder($conn);
+$seeder->fillUsers(10);
